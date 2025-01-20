dda9e8e663ac48e5ca2331a0ac9addb47f55b73e
SattyaP <stylearning7@gmail.com>
2025-01-21 05:37:03 +0700

    fix: modify snap.js

--- resources/views/subscribe/order.blade.php
+++ resources/views/subscribe/order.blade.php
@@ -94,6 +94,6 @@
         }
     </script>
     {{-- <script src="https://app..midtrans.com/snap/snap.js" data-client-key="YOUR_CLIENT_KEY"></script> --}}
-    <script src="https://app.midtrans.com/snap/snap.js" data-client-key="{{ config('services.midtrans.client_key') }}">
+    <script src="https://app.midtrans.com/snap/snap.js" data-client-key="{{ config('services.midtrans.clientKey') }}">
     </script>
 @endsection()
